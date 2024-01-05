5adc3d020cb0826de641365147ba372494dbd34e
李大狗 <leeduckgo@gmail.com>
2024-01-05 17:15:45 +0800

    Initial commit

---
+++ components/TheButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="rounded-full p-px border bg-third-gray"
+    :class="[coloredBorder && 'bg-gradient-to-r from-grdb via-grdm to-grde']" @mouseover="hover = true"
+    @mouseleave="hover = false">
+    <button class="rounded-full bg-white dark:bg-black px-8 text-black-2 dark:text-white text-[15x] font-['Montserrat']"
+      :class="[high ? 'py-4' : 'py-2.5', uppercase && 'uppercase', classText]">
+      {{ btnText }}
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const props = defineProps({
+  classText: {
+    type: String,
+    default: '',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  uppercase: {
+    type: Boolean,
+    default: false,
+  },
+  high: {
+    type: Boolean,
+    default: false,
+  },
+  coloredBorder: {
+    type: Boolean,
+    default: false,
+  },
+  changeText: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const hover = ref(false)
+const btnText = computed(() => {
+  return (hover.value && props.changeText) ? 'Coming soon' : props.text
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="flex flex-col w-content mx-auto pt-9">
+    <div class="header h-16 flex justify-between items-center">
+      <div class="flex items-center space-x-3">
+        <img src="/img/logo.png" alt="logo" class="h-12" />
+        <img src="/img/logo-title.png" alt="logo" class="h-6 dark:brightness-0 dark:invert" />
+      </div>
+      <div class="links flex items-center space-x-5 text-black-1 dark:text-white font-['Poppins'] text-[17px]">
+        <a v-for="link in links" :key="link" class="group relative h-7 flex items-center cursor-pointer">
+          <span>{{ link }}</span>
+          <img class="w-6 h-6 dark:brightness-0 dark:invert" src="~/assets/svg/down-select.svg" alt="select">
+          <p
+            class="hidden group-hover:flex absolute -bottom-3 left-1/2 -translate-x-1/2 w-[57px] h-3 rounded-[2px] bg-[#FFDC02] justify-center items-center text-[8px] font-['Poppins']">
+            Coming soon
+          </p>
+        </a>
+        <div class="flex justify-center ml-2" @click="toggleColorMode">
+          <input type="checkbox" name="light-switch" class="light-switch sr-only" />
+          <label
+            class="relative h-[30px] p-[3px] bg-[#F0F2F5] dark:bg-[#2C2C2C] rounded-full after:content-[''] after:transition-transform after:duration-500 after:absolute after:dark:translate-x-full after:w-6 after:h-6 after:p-[6px] after:rounded-full after:bg-gradient-to-r after:from-[#439DFF] after:to-[#6052FF] flex items-center cursor-pointer select-none"
+            for="light-switch">
+            <img class="m-[6px] z-20 w-3 h-3" src="~/assets/svg/light-mode.svg" alt="light-mode">
+            <img class="m-[6px] z-20 w-3 h-3" src="~/assets/svg/dark-mode.svg" alt="dark-mode">
+            <span class="sr-only">Switch to light / dark version</span>
+          </label>
+        </div>
+      </div>
+    </div>
+    <div class="slogon mt-12 flex justify-between items-center">
+      <div class="flex flex-col space-y-12">
+        <div class="texts flex flex-col space-y-6">
+          <div class="titles flex flex-col space-y-9">
+            <div class="flex flex-col space-y-4 text-black-1 dark:text-white">
+              <span class="w-[450px] text-[72px] leading-none font-['Poppins'] font-medium tracking-[0.6px]">Web 3 AI
+                Powerhouse</span>
+              <span class="w-[510px] text-[36px] leading-tight font-['Poppins']">For Search, AIGC, Data
+                Marketplace, and Apps</span>
+            </div>
+            <LinearLine classText="w-[615px]" />
+          </div>
+          <div class="flex items-center space-x-2 text-third-gray dark:text-gray-4 text-[21px]">
+            <span class="leading-8">Powered by BNB Greenfield</span>
+            <img class="w-8 h-8" src="/img/logo-bnb.png" alt="bnb">
+          </div>
+        </div>
+        <div class="buttons flex items-center space-x-8">
+          <template v-for="button in buttons" :key="button">
+            <TheButton :text="button.text" :uppercase="true" :high="true" :colored-border="button.coloredBorder"
+              :change-text="true" />
+          </template>
+        </div>
+      </div>
+      <img class="w-[568px]" src="/img/dataism.png" alt="dataism">
+    </div>
+    <div class="mt-[72px]">
+      <img class="mx-auto h-[30px] dark:brightness-0 dark:invert" src="/img/clients.png" alt="clients">
+    </div>
+    <div class="mt-[26px] text-center text-third-gray dark:text-gray-5 text-[20px] font-['Poppins']">
+      Trusted by and integrated with the world’s most ambitious teams
+    </div>
+    <div class="mt-40 mx-auto w-[1160px] flex flex-col items-center space-y-[142px]">
+      <div class="w-full flex justify-start items-center space-x-60">
+        <div class="w-[555px] flex flex-col space-y-6 items-start">
+          <div class="w-full flex flex-col space-y-2 text-[20px] text-second-gray dark:text-white font-['Poppins']">
+            <span>MOVESPACE is an ecosystem of applications powered by the MoveSpace vector datahouse. We utilize
+              decentralized storage solutions including BNB Greenfield and Arweave to power on-chain data labeling, data
+              monetization, AI-powered search, AIGC & AI-based UGC, NFT, and gaming use cases.</span>
+            <span>Our team consists of Web 3 natives and veteran builders from Tencent, Huawei, Microsoft, Google, and
+              Netease, etc.</span>
+          </div>
+          <div class="w-[575px] p-2.5">
+            <LinearLine />
+          </div>
+          <TheButton text="Learn more" :colored-border="true" @click="scrollToTop" />
+        </div>
+        <div class="title text-black-2 dark:text-white text-[40px] font-['Poppins'] font-semibold">ABOUT US</div>
+      </div>
+      <div class="w-full flex justify-start items-center space-x-60">
+        <div class="title w-[555px] text-black-2 dark:text-white text-[40px] font-['Poppins'] font-semibold">APPLICATIONS
+        </div>
+        <div class="w-[555px] flex flex-col space-y-6 items-start">
+          <div class="w-full flex flex-col space-y-2 text-[20px] text-second-gray dark:text-white font-['Poppins']">
+            <span>The MoveSpace ecosystem is a set of applications built on MoveSpace’s vector datahouse.</span>
+            <ul class="list-disc">
+              <li>MoveSpace Data Explorer: a vector-data-powered search engine to power Web 3 native search, beginning
+                with Galxe & Bodhi.</li>
+              <li>MoveSpace Data Marketplace: a data publishing and marketplace platform built on BNBChain & BNB
+                Greenfield.</li>
+              <li>MoveSpace AI Dashboard: a multi-modal AIGC platform that allows users to upload data on-chain and
+                generate
+                AIGC based on their textual data and image data.</li>
+              <li>MoveSpace Data Labeling Platform: an on-chain data labeling platform for Large language models and
+                applications, built on BNBChain and BNB Greenfield.</li>
+            </ul>
+          </div>
+          <div class="w-[575px] p-2.5">
+            <LinearLine />
+          </div>
+          <TheButton text="Learn more" :colored-border="true" @click="scrollToTop" />
+        </div>
+      </div>
+    </div>
+    <div class="footer mt-60 pb-24 flex flex-col space-y-3">
+      <LinearLine />
+      <div class="flex justify-between items-center text-third-gray dark:text-white font-['Montserrat']">
+        <span v-for="text in footerTexts" :key="text">{{ text }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const links = [
+  'Data Explorer',
+  'Data Marketplace',
+  'AI Dashboard',
+  'Data Labeling Platform',
+]
+const buttons = [
+  {
+    text: 'testnet',
+    coloredBorder: true,
+  },
+  {
+    text: 'read docs',
+    coloredBorder: false,
+  },
+]
+const footerTexts = [
+  'Copyright © 2023 MOVESPACE. All rights reserved.',
+  'Terms of Use & Privacy Policy',
+]
+
+const colorMode = useColorMode()
+const toggleColorMode = () => {
+  if (colorMode.preference === 'dark') {
+    colorMode.preference = 'light'
+  } else {
+    colorMode.preference = 'dark'
+  }
+}
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })
+}
+</script>
+
+<style scoped></style>
